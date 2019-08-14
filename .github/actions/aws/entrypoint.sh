baac593e7443a65ff4920eb141ba4bcc3dbc392b
Mengwei Ding <mengwei.ding@gmail.com>
2019-08-14 14:36:34 -0700

    use container to run job (#2)
    * use container to run job
    
    * test env
    
    * test env
    
    * test env
    
    * split jobs

--- .github/actions/aws/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo 'Deploy to S3'
-aws s3 sync public s3://mengwei.me --region=us-west-1 --delete
--- .github/actions/hugo/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo 'Build Site'
-hugo
-
-echo 'List the built output directory'
-ls -al ./public
